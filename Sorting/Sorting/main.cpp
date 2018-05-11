11b7ad06f4b267fdbefb0a3575b18545c700e8b7
appalse <Natalia.kutina13@gmail.com>
2018-05-11 19:17:04 +0300

    Add selection sort

--- Sorting/Sorting/main.cpp
+++ Sorting/Sorting/main.cpp
@@ -16,14 +16,41 @@ void InsertionSort(T data[], size_t len) {
 				std::swap(data[j], data[j - 1]);
 }
 
+template <typename T>
+void SelectionSort(T data[], size_t len) {
+	for (size_t i = 0; i < len - 1; ++i) {
+		T minItem = data[i];
+		size_t minIndex = i;
+		for (size_t j = i + 1; j < len; ++j)
+			if (data[j] < minItem) {
+				minItem = data[j];
+				minIndex = j;
+			}
+		if (minIndex != i)
+			std::swap(data[i], data[minIndex]);
+	}
+}
+
 
 
 int main() {
-	int data[] = {5, 4, 3, 1, 2, 10};
-	auto len = sizeof(data) / sizeof(data[0]);
-	Print(&data[0], len);
-	InsertionSort(&data[0], len);
-	Print(&data[0], len);
+	{
+		std::cout << "Insertion sort:" << std::endl;
+		int data[] = { 5, 4, 3, 1, 2, 10 };
+		auto len = sizeof(data) / sizeof(data[0]);
+		Print(&data[0], len);
+		InsertionSort(&data[0], len);
+		Print(&data[0], len);
+	}
+
+	{
+		std::cout << "Selection sort:" << std::endl;
+		int data[] = { 5, 4, 3, 1, 2, 10 };
+		auto len = sizeof(data) / sizeof(data[0]);
+		Print(&data[0], len);
+		SelectionSort(&data[0], len);
+		Print(&data[0], len);
+	}
 	system("pause");
 	return 0;
 }
